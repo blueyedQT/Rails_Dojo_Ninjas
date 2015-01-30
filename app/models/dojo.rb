eb216d861667e746a3bf99921c2f86c90a558ea6
Katrina Sanford <blueyed_qt@hotmail.com>
2015-01-29 18:07:06 -0800

    Final commit!

--- app/models/dojo.rb
+++ app/models/dojo.rb
@@ -1,6 +1,5 @@
 class Dojo < ActiveRecord::Base
-	has_many :ninjas
-	# , dependant: :destroy
+	has_many :ninjas, dependent: :destroy
 	validates :name, :city, presence: true
 	validates :state, presence: true, length: { is: 2 }
 end
